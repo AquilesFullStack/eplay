373110cb09976aa178afb01d8bee10174a39bc02
Aquiles <aquilesnetto2002@gmail.com>
2024-11-14 16:02:29 -0300

    Projeto finalizado

--- src/pages/Categories/index.tsx
+++ src/pages/Categories/index.tsx
@@ -1,19 +1,18 @@
-import Banner from "../../components/Banner"
-import ProductsList from "../../components/ProductsList"
+import ProductsList from '../../components/ProductsList'
 
 import resident from '../../assets/images/resident.png'
 import diablo from '../../assets/images/diablo.png'
 import starwars from '../../assets/images/starwars.png'
 import streetfighter from '../../assets/images/streetfighter.png'
 import zelda from '../../assets/images/zelda.png'
-import Game from "../../models/Game"
-
+import Game from '../../models/Game'
 
 const promocoes: Game[] = [
   {
     id: 1,
     category: 'Ação',
-    description: 'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
     title: 'Resident Evil 4',
     system: 'Windows',
     infos: ['10%', 'R$ 250,00'],
@@ -22,7 +21,8 @@ const promocoes: Game[] = [
   {
     id: 2,
     category: 'Ação',
-    description: 'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
     title: 'Resident Evil 4',
     system: 'PS5',
     infos: ['5%', 'R$ 290,00'],
@@ -31,7 +31,8 @@ const promocoes: Game[] = [
   {
     id: 3,
     category: 'Ação',
-    description: 'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
     title: 'Resident Evil 4',
     system: 'Windows',
     infos: ['10%', 'R$ 250,00'],
@@ -40,7 +41,8 @@ const promocoes: Game[] = [
   {
     id: 4,
     category: 'Ação',
-    description: 'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
     title: 'Resident Evil 4',
     system: 'Windows',
     infos: ['10%', 'R$ 250,00'],
@@ -52,47 +54,52 @@ const emBreve: Game[] = [
   {
     id: 5,
     category: 'RPG',
-    description: 'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
     title: 'Diablo 4',
     system: 'Windows',
     infos: ['17/05'],
-    image: diablo,    
+    image: diablo
   },
   {
     id: 6,
     category: 'RPG',
-    description: 'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
     title: 'Zelda',
     system: 'Windows',
     infos: ['17/05'],
-    image: zelda,
+    image: zelda
   },
   {
     id: 7,
     category: 'RPG',
-    description: 'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
     title: 'Star Wars',
     system: 'Windows',
     infos: ['17/05'],
-    image: starwars,    
+    image: starwars
   },
   {
     id: 8,
     category: 'RPG',
-    description: 'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
     title: 'Resident Evil 4',
     system: 'Windows',
     infos: ['17/05'],
-    image: resident,    
-  },
+    image: resident
+  }
 ]
 
-const Home = () => (
+const Categories = () => (
   <>
-    <Banner />
-    <ProductsList games={promocoes} title="Promoções" background="gray" />
-    <ProductsList games={emBreve} title="Em breve" background="black" />
+    <ProductsList games={promocoes} title="RPG" background="gray" />
+    <ProductsList games={emBreve} title="Ação" background="black" />
+    <ProductsList games={promocoes} title="Aventura" background="gray" />
+    <ProductsList games={emBreve} title="FPS" background="black" />
   </>
 )
 
-export default Home
+export default Categories
